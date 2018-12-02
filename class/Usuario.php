12d0008ad0154cb45a4fa1eab4a82a1f8aa53635
ttalessoft <ttalessoft@gmail.com>
2018-12-02 16:12:16 -0200

    funcoes em dao

--- class/Usuario.php
+++ class/Usuario.php
@@ -35,6 +35,7 @@
             $this->dtcadastro = $dtcadastro;
         }
 
+        // pega apenas um usuário a partir do parametro
         public function loadbyId($id){
             
             $sql = new Sql();
@@ -54,6 +55,47 @@
 
             }
 
+        }
+
+        // busca o usuario pelo login
+        public static function search($login){
+
+            $sql = new Sql();
+
+            return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+                ':SEARCH'=>'%' . $login . '%'
+            ));
+        }
+
+        // pega a lista de todos os usuarios é static pois nao tem $this no metodo
+        public static function getList(){
+
+            $sql = new Sql();
+
+            return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+        }
+
+        // valida um acesso com credenciais
+        public function login($login, $password){
+
+            $sql = new Sql();
+
+            $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :DESLOGIN AND dessenha = :DESSENHA", array(
+                ":DESLOGIN"=>$login,
+                ":DESSENHA"=>$password
+            ));
+
+            if (count($results) > 0) {
+                $row = $results[0];
+
+                $this->setIdusuario($row['idusuario']);
+                $this->setDeslogin($row['deslogin']);
+                $this->setDessenha($row['dessenha']);
+                $this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+            }else{
+                throw new Exception("Login e/ou Senha inválidos!");
+            }
         }
 
         public function __toString(){
--- index.php
+++ index.php
@@ -4,6 +4,16 @@
 
     $usr = new Usuario();
 
-    $usr->loadbyId(2);
+    // carrega apenas um usuario
+    //$usr->loadbyId(2);
+
+    // carrega todos os usuários
+    //$lista = Usuario::getList();
+
+    // carrega uma busca pelo nome do usuario
+    //$lista = Usuario::search("jose");
+
+    // valida uma tentativa de login
+    $usr->login("user", "senha1234");
 
     echo $usr;
